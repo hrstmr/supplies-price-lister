37780c1ab2cd0f10e7fc2930bc9992c24bd70bea
hrstmr <37159938+hrstmr@users.noreply.github.com>
2021-10-26 18:27:09 +1100

    Added Supply Model

---
+++ Model/Supply.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace buildxact_supplies.Model
+{
+    public class Supply
+    {
+        //after convertion from csv & json id might not be unique, need more context about the file, can filename+Id be unique or can these file or duplicate file can be generated over time
+        public string Id { get; set; }
+        public string Currency { get; set; } //Can be a currency class with {code:"AUD" , symbol:"$", desc:"australian dollar"}
+        public string Description { get; set; }
+        public string Unit { get; set; } //Can be a Unit class with {type:"Length",name:"meter",symbol:"m"}
+        public decimal UnitPrice { get; set; }
+    }
+}
