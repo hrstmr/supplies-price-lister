5cf1ef771c89db62f455319ccb2d3df908766891
hrstmr <37159938+hrstmr@users.noreply.github.com>
2021-10-26 18:35:45 +1100

    Added Entity class from Supply - used CSVHelper

---
+++ Entities/HumphriesSupply.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CsvHelper.Configuration.Attributes;
+
+namespace buildxact_supplies.Entities
+{
+    public class HumphriesSupply
+    {
+        //name attributes to map the csv field to HumphriesSupply property
+        [Name("identifier")]
+        public string Id { get; set; } //its not mentioned in the doc that this is unique
+        [Name("desc")]
+        public string Description { get; set; }
+        [Name("unit")]
+        public string Unit { get; set; }
+        [Name("costAUD")]
+        public decimal Price { get; set; }
+    }
+}
