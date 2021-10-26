0d8949903eed4d880aa3c8bb8e512ef822049c6b
hrstmr <37159938+hrstmr@users.noreply.github.com>
2021-10-26 19:35:20 +1100

    Added Supply Repository

---
+++ Repositories/ISupplyRepository.cs
@@ -0,0 +1,14 @@
+﻿using buildxact_supplies.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buildxact_supplies.Repositories
+{
+    public interface ISupplyRepository
+    {
+        Task<IEnumerable<Supply>> GetFromFile(string filePath);
+        Task<IEnumerable<Supply>> GetAll();
+    }
+}
---
+++ Repositories/SupplyRepository.cs
@@ -0,0 +1,21 @@
+﻿using buildxact_supplies.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buildxact_supplies.Repositories
+{
+    public class SupplyRepository : ISupplyRepository
+    {
+        public Task<IEnumerable<Supply>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Supply>> GetFromFile(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
