fd75d5cb7c7b7f3b9d24b6a927d72e2467f3c4ef
hrstmr <37159938+hrstmr@users.noreply.github.com>
2021-10-26 19:52:58 +1100

    Added Functionality to Display Supplies

---
+++ Controllers/SupplyController.cs
@@ -0,0 +1,38 @@
+﻿using buildxact_supplies.Model;
+using buildxact_supplies.Repositories;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace buildxact_supplies
+{
+    public class SupplyController
+    {
+        private readonly ISupplyRepository _supplyRepository;
+        public SupplyController(ISupplyRepository supplyRepository)
+        {
+            _supplyRepository = supplyRepository;
+        }
+
+        // This should be an seperate class
+        public async void DisplaySupplies()
+        {
+            var supplies = await GetAllSupply();
+            foreach (var supply in supplies)
+            {
+                var currencySymbol = "$";
+                Console.WriteLine($"{supply.Id},{supply.Description},{currencySymbol}{decimal.Round(supply.UnitPrice, 2)}");
+            }
+            Console.ReadLine();
+        }
+
+        public async Task<IEnumerable<Supply>> GetAllSupply()
+        {
+            //find a way avoid hardcoping the sort and use sort Request
+            var supplies = await _supplyRepository.GetAll();
+            return supplies.OrderByDescending(s => s.UnitPrice);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,17 @@
-﻿using buildxact_supplies.Entities;
+﻿using buildxact_supplies;
+using buildxact_supplies.Entities;
 using buildxact_supplies.Helper;
 using buildxact_supplies.Mappers;
+using buildxact_supplies.Repositories;
 using CsvHelper;
 using CsvHelper.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using System;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System;
-using buildxact_supplies.Helper;
-using buildxact_supplies.Repositories;
-using Newtonsoft.Json;
-using Microsoft.Extensions.DependencyInjection;
-using buildxact_supplies;
+
 namespace SuppliesPriceLister
 {
     class Program
@@ -21,6 +19,9 @@ namespace SuppliesPriceLister
         static void Main(string[] args)
         {
             var serviceProvider = ConfigureDependancies();
+            var supplyController = serviceProvider.GetService<SupplyController>();
+            supplyController.DisplaySupplies();
+            Console.ReadLine();
         }
 
         public static IServiceProvider ConfigureDependancies()
